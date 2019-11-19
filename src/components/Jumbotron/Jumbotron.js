28aedaf172902798c3df9f1cff9fdd3470e1e025
Jenny Ivy <47339542+jenivy06@users.noreply.github.com>
2019-11-19 10:48:09 -0700

    completed all code to update to avengers characters

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -5,8 +5,8 @@ import "./Jumbotron.css";
 
 const Jumbotron = () => (
 	<header className = "header">
-		<h1>"The Simpsons" Clicky Game</h1>
-		<h2>Click on any character's image to earn a point; but don't click on the same character more than once! Click all 12 pics, and you win!</h2>
+		<h1>"AVENGERS" Memory Game</h1>
+		<h2>Click on any character's image to earn a point; but don't click on the same character more than once! Click all 12 pics only once, and you win!</h2>
 	</header>
 );
 
