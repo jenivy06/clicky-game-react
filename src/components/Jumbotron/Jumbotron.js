d97e7d19fcca3e295b616c11255fc85223b6ba0e
Jenny Ivy <47339542+jenivy06@users.noreply.github.com>
2019-11-19 12:19:47 -0700

    completed all code to change characters to pooh

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -5,7 +5,7 @@ import "./Jumbotron.css";
 
 const Jumbotron = () => (
 	<header className = "header">
-		<h1>"AVENGERS" Memory Game</h1>
+		<h1>"Winnie the Pooh" Memory Game</h1>
 		<h2>Click on any character's image to earn a point; but don't click on the same character more than once! Click all 12 pics only once, and you win!</h2>
 	</header>
 );
